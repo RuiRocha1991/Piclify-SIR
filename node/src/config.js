4f729ec3dfc4444aba8bac6209c6742c5499343b
Rui Rocha <ruirocha1991@gmail.com>
2018-12-29 15:42:42 +0000

    Register account finished

--- node/src/config.js
+++ node/src/config.js
@@ -1,4 +1,4 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.SALT_KEY = 'BD3583FB4F1EF44829DA1199EB3AF';
 global.EMAIL_TMPL='Olá <strong>{0}</strong> seja bem vindo ao node store';
 module.exports={
     sendgridkey:'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'
